264299f010a9f0648addb4ef9b1f622b8cec29d5
Nick Pilon <npilon@lexmachina.com>
2016-10-14 15:05:53 -0700

    Initial best effort decoder tween with tests

--- pyramid_unicodedammit/__init__.py
+++ pyramid_unicodedammit/__init__.py
@@ -1,2 +1,28 @@
+import codecs
+
+from bs4 import UnicodeDammit
+
+from pyramid.tweens import INGRESS
+
+
+def unicodedammit_tween_factory(handler, registry):
+
+    def unicodedammit_tween(request):
+        env = request.environ
+        qs = env.get('QUERY_STRING', '')
+
+        if qs:
+            try:
+                codecs.utf_8_decode(qs, 'strict', True)
+            except UnicodeDecodeError:
+                ud = UnicodeDammit(qs)
+                env['QUERY_STRING'] = codecs.encode(ud.unicode_markup, 'utf-8')
+
+        return handler(request)
+
+    return unicodedammit_tween
+
+
 def includeme(config):
-    pass
+    config.add_tween('pyramid_unicodedammit.unicodedammit_tween_factory',
+                     under=INGRESS)
